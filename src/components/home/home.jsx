0f130e6ef8576d817339f7dbcf1dac0139e0a31e
jjieunyun <lesson0186@gmail.com>
2022-06-07 13:46:56 +0900

    CSS about

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -5,15 +5,17 @@ import avatar from '../../imgs/avatar.png'
 
 const Home = () => {
     return (
-        <section id="home">
+        <section className={styles.home}>
             <img className={styles.background} src={background} alt="" />
             <div className={styles.avatar}>
                 <img  src={avatar} alt="avatar"/>
             </div>
             <div className={styles.intro}>
-                <h1 className={styles.title}>Hello! I'm Jieun Yun</h1>
-                <h3>I like making fun, interactive things with code. I also talk & write about those things.</h3>
-                <button>Contact Me</button>
+                <h1 className={styles.title}>Hello! I'm jieun Yun &#128077;</h1>
+                <h3><span>Positive</span>긍정적인 마인드와</h3>
+                <h3><span>passion</span>열정가득한 도전정신으로</h3>
+                <h3><span>cooperation</span>함께 할때, 더욱 빛나는 Frontend Developer입니다.</h3>
+                <button className={styles.button}>Contact Me</button>
             </div>
         </section>
         
