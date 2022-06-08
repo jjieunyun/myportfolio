0336aab1d4a5bf6fd4fdb26fb14e8c9eda645134
jieun yun <lesson0186@gmail.com>
2022-06-08 14:03:05 +0900

    navbar JS

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -4,14 +4,16 @@ import styles from './contact.module.css'
 const Contact = () => {
     return (
         <section className={styles.contact}>
-            <h1 className={styles.title}>Let's Talk</h1>
-            <h2 className={styles.email}>jjieunyun@naver.com</h2>
-            <a href='https://github.com/'>
-                아이콘
-            </a>
-            <a href='https://github.com/'>
-                아이콘
-            </a>
+            <h1 className={styles.title}>Contact</h1>
+            <div className={styles.container}>
+                <h2 className={styles.email}>jjieunyun@naver.com</h2>
+                <a href='https://github.com/'>
+                    아이콘
+                </a>
+                <a href='https://github.com/'>
+                    아이콘
+                </a>
+            </div>
         </section>
     );
 };
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,39 +1,55 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import styles from './navbar.module.css'
 
 const Navbar = () => {
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    const updateScroll = () => {
+        setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+    }
+
+    useEffect(()=>{
+        window.addEventListener('scroll', updateScroll);
+    });
+    
     return (
-        <nav className={styles.navbar}>
-            <div className={styles.navbar_logo}>
-                <a href="" className={styles.title}>
+        <nav className={`${scrollPosition < 100? styles.navbar : styles.changed_navbar}`}>
+            <div 
+            className={`${scrollPosition < 100? styles.navbar_logo : styles.changed_logo}`}>
+                <a href='www.naver.com' 
+                    className={`${scrollPosition < 100? styles.title : styles.changed_title}`}>
                     <p>jieun.Yun</p>
                 </a>
             </div>
 
-
-            <ul className={styles.navbar_menu}>
-                <li className={styles.navbar_menu_item}
+            <ul 
+            className={`${scrollPosition < 100? styles.navbar_menu : styles.changed_menu}`}>
+                <li 
+                    className={`${scrollPosition < 100? styles.navbar_menu_item : styles.changed_item}`}
                     data-text='Home'>
                     Home
                 </li>
-                <li className={styles.navbar_menu_item}
+                <li 
+                    className={`${scrollPosition < 100? styles.navbar_menu_item : styles.changed_item}`}
                     data-text='About'>
                     About
                 </li>
-                <li className={styles.navbar_menu_item}
+                <li 
+                    className={`${scrollPosition < 100? styles.navbar_menu_item : styles.changed_item}`}
                     data-text='Skills'>
                     Skills
                 </li>
-                <li className={styles.navbar_menu_item}
+                <li 
+                    className={`${scrollPosition < 100? styles.navbar_menu_item : styles.changed_item}`}
                     data-text='Myworks'>
                     Myworks
                 </li>
-                <li className={styles.navbar_menu_item}
+                <li 
+                    className={`${scrollPosition < 100? styles.navbar_menu_item : styles.changed_item}`}
                     data-text='Contact'>
                     Contact
                 </li>
             </ul>
-
         </nav>
     );
 };
