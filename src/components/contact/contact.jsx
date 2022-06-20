819a93e2f56ca1128ca1a23854caa714f575b5c1
jieun yun <lesson0186@gmail.com>
2022-06-20 13:55:39 +0900

    contact꾸미는중

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,6 +1,8 @@
 import React, { forwardRef } from 'react';
+import GithubButton from '../../github-button/GithubButton';
 import Mouse from '../mouse';
-import styles from './contact.module.css'
+import styles from './contact.module.css';
+
 
 
 const Contact = forwardRef((props,ref) => {
@@ -8,6 +10,10 @@ const Contact = forwardRef((props,ref) => {
         <section ref={ref} className={styles.contact}>
             <h1 className={styles.title}>Contact</h1>
             <div className={styles.container}>
+            <h3>지금까지 제 포트폴리오를 읽어주셔서 감사합니다. </h3>
+            <h3>아래는 공부하는 내용을 꾸준히 기록하고 있는 저의 GitHub와 Notion링크 입니다😊</h3>
+            
+            <GithubButton/>
                 <div className={styles.icon_container}>
                     <a className={styles.icon} href='https://github.com/'>
                         <i class="fa-solid fa-envelope"></i>
@@ -18,18 +24,9 @@ const Contact = forwardRef((props,ref) => {
                         <i class="fa-solid fa-phone"></i>
                     </a>
                 </div>
-                <div className={styles.icon_container}>
-                    <a className={styles.icon} href='https://github.com/'>
-                        <i class="fa-brands fa-github"></i>
-                    </a>
-                </div>
-                <div className={styles.icon_container}>
-                    <a className={styles.icon} href='https://github.com/'>
-                        <i  class="fa-solid fa-cube"></i>
-                    </a>
-                </div>
             </div>
-            <Mouse/>
+{/* 
+            <Mouse/> */}
         </section>
     );
 });
